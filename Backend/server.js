f6a98ba61772d96022e5adf4c0485cefa5c40105
Kani1902 <kanishka05.m@gmail.com>
2025-07-09 22:42:40 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -22,7 +22,7 @@ app.get('/health', (req, res) => {
   res.send("✅ Server healthy!");
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
